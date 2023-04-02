e7d53f9caad775f31a5b31dd55fce3a906749d4b
Sebastian <kaynandredblue@gmail.com>
2023-04-02 17:25:44 -0500

    fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv->safeLoad();
 
 $name = $_ENV['NAME'];
 $year = $_ENV['YEAR'];

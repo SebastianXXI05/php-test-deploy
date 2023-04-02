b602e8fb9ded4577dd56bb7055ab4bec8da27e5e
Sebastian <kaynandredblue@gmail.com>
2023-04-02 16:18:30 -0500

    start

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+require_once __DIR__.'/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$name = $_ENV['NAME'];
+$year = $_ENV['YEAR'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test deploy php with composer</title>
+</head>
+<body>
+    <h2>Name: <?= $name ?></h2>
+    <h2>Year: <?= $year ?></h2>
+</body>
+</html>
